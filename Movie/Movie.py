48c651d4c189f338b6a0e74ad052a07df40094f5
Roberto <reybrujo@gmail.com>
2019-07-13 01:20:20 -0300

    Added docstrings

--- Movie/Movie.py
+++ Movie/Movie.py
@@ -1,5 +1,8 @@
+"""Movie.py"""
+
 class Movie(object):
     def __init__(self, identifier: int, title: str, description: str, releasedate: str, director: str, category: str):
+        """Constructor"""
         self.identifier = identifier
         self.title = title
         self.description = description
@@ -8,4 +11,5 @@ class Movie(object):
         self.category = category
 
     def __str__(self):
+        """String"""
         return 'Id: {0}, Titulo: {1}, Desc: {2}, Fecha: {3}, Autor: {4}'.format(self.identifier, self.title, self.description, self.releasedate, self.director)
