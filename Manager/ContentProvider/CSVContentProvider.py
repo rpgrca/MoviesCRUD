27ddca95cb166423627e5ebe162e209945b646e9
Roberto Alfonso <robertoa@prx.com.ar>
2019-07-15 18:48:18 -0300

    CSV support implemented

--- Manager/ContentProvider/CSVContentProvider.py
+++ Manager/ContentProvider/CSVContentProvider.py
@@ -18,7 +18,11 @@
 """FileContentProvider.py"""
 
 from typing import List, Any
+import csv
+import os
 from Manager.ContentProvider.ContentProvider import ContentProvider
+from Movie.Movie import Movie
+from Movie.MoviesFactory import MoviesFactory
 
 class CSVContentProvider(ContentProvider):
     def __init__(self, filename: str):
@@ -27,6 +31,7 @@ class CSVContentProvider(ContentProvider):
         self.extra_data = "movies.csv"
         self.key = CSVContentProvider.KEY()
         self.__filename = filename if filename else self.extra_data
+        self.__delimiter = '\t'
 
     @staticmethod
     def KEY() -> str:
@@ -34,9 +39,35 @@ class CSVContentProvider(ContentProvider):
         return "csv"
 
     def load(self):
-        # TODO: Cargar items del archivo
-        pass
+        if self.__filename and not self.initialized:
+            if os.path.isfile(self.__filename):
+                with open(self.__filename, "r") as input_file:
+                    reader = csv.reader(input_file, delimiter=self.__delimiter)
+                    for line in reader:
+                        try:
+                            if line:
+                                identifier, title, description, releasedate, director, category = line
+                                if identifier and identifier != 'identifier':
+                                    movie = MoviesFactory.create1(int(identifier), title, description, releasedate, director, category)
+                                    self.movies.append(movie)
+
+                                    if category not in self.categories:
+                                        self.categories.append(category)
+                                else:
+                                    pass # El archivo csv tiene header
+                        except:
+                            # TODO: Agregar numero de linea
+                            raise ValueError("Formato invalido en archivo CSV")
+
+            self.initialized = True
+        
 
     def save(self):
-        # TODO: Grabar items del archivo
-        pass
+        if self.__filename and self.initialized:
+            with open(self.__filename, "w", newline='') as output_file:
+                writer = csv.DictWriter(output_file, delimiter=self.__delimiter, fieldnames=Movie.getHeaders())
+                writer.writeheader()
+                for movie in self.movies:
+                    writer.writerow(movie.toDictionary())
+
+            self.initialized = False
