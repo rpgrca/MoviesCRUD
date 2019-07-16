1e4af51b6c2e60564883da1f110253619efe0fad
Roberto Alfonso <robertoa@prx.com.ar>
2019-07-16 15:26:32 -0300

    Added docstrings

--- Manager/Categories/CategoriesManager.py
+++ Manager/Categories/CategoriesManager.py
@@ -5,6 +5,8 @@ from Manager.ItemsManager import ItemsManager
 from Manager.ContentProvider.ContentProvider import ContentProvider
 
 class CategoriesManager(ItemsManager):
+    """Manejador de categorias, mantiene la lista de categorias existentes en memoria"""
+
     def __init__(self, content_provider: ContentProvider):
         """Constructor"""
         super(CategoriesManager, self).__init__(content_provider)
@@ -26,12 +28,14 @@ class CategoriesManager(ItemsManager):
         return super(CategoriesManager, self).items
 
     def create(self, category: str) -> str:
+        """Crea una nueva categoria si no existe dentro de la lista"""
         if category not in self.categories:
             self.categories.append(category)
 
         return category
 
     def get_category_index(self, category: str) -> int:
+        """Retorna el indice de la categoria indicada en la lista"""
         index = self.categories.index(category)
         if index >= len(self.categories):
             index = -1
