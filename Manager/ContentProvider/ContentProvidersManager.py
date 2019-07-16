9b5ffd2c2545057daffa00d7b98b1ffdb4a93ae1
Roberto Alfonso <robertoa@prx.com.ar>
2019-07-16 14:27:14 -0300

    Create and append provider if does not exist

--- Manager/ContentProvider/ContentProvidersManager.py
+++ Manager/ContentProvider/ContentProvidersManager.py
@@ -6,18 +6,24 @@ from Manager.ContentProvider.ContentProvider import ContentProvider
 from Manager.ContentProvider.ContentProviderFactory import ContentProviderFactory
 
 class ContentProvidersManager(ItemsManager):
+    """Manejador de Content Providers, posee una instancia de cada uno en memoria"""
+
     def __init__(self):
         """Constructor"""
         super(ContentProvidersManager, self).__init__(None)
 
     def create(self, content_provider: str, extra_data: str) -> ContentProvider:
+        """Retorna el content provider indicado, creandolo en caso de no existir"""
         result = [x for x in self.items if x.key == content_provider]
         if result:
             return result[0]
         else:
-            return ContentProviderFactory.create(content_provider, extra_data)
+            result = ContentProviderFactory.create(content_provider, extra_data)
+            self.items.append(result)
+            return result
 
     def load(self):
+        # TODO: Podria ser a pedido para optimizar la creacion
         """Carga cada uno de los content providers disponibles"""
         content_providers = []
         for provider in ContentProviderFactory.get_providers():
