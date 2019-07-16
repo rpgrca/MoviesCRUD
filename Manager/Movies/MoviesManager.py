89d478e3255f79c29fce7a05533e37b4d79461c9
Roberto Alfonso <robertoa@prx.com.ar>
2019-07-16 14:18:06 -0300

    Added class docstring, couple of TODO comments

--- Manager/Movies/MoviesManager.py
+++ Manager/Movies/MoviesManager.py
@@ -6,6 +6,8 @@ from Manager.ContentProvider.ContentProvider import ContentProvider
 from Movie.Movie import Movie
 
 class MoviesManager(ItemsManager):
+    """Manejador de peliculas, mantiene la lista de peliculas actualmente visualizadas por el formulario"""
+
     def __init__(self, content_provider: ContentProvider):
         """Constructor"""
         super(MoviesManager, self).__init__(content_provider)
@@ -26,16 +28,18 @@ class MoviesManager(ItemsManager):
         """Retorna la lista actual de peliculas del Manager"""
         return super(MoviesManager, self).items
 
+    # TODO: Ya existe esta funcion en MoviesFactory, consolidar
     def create(self, title: str, description: str, releasedate: str, director: str, category: str) -> Movie:
         """Crea una pelicula con los datos dados"""
         return Movie(self.get_next_identifier(), title, description, releasedate, director, category)
 
+    # TODO: Usualmente se retorna el elemento eliminado
     def remove(self, identifier: int):
         """Borra la pelicula con el identificador dado del Manager"""
         self.items = list(filter(lambda x : x.identifier != identifier, self.items))
 
     def get_movie(self, identifier: int) -> Movie:
-        """Retorna la pelicula cuyo identificador ha sido dado"""
+        """Retorna la pelicula cuyo identificador es igual al indicado"""
         result = list(filter(lambda x : x.identifier == identifier, self.items))
         if result and len(result) > 0:
             return result[0]
@@ -43,7 +47,7 @@ class MoviesManager(ItemsManager):
         return None
 
     def get_next_identifier(self) -> int:
-        """Retorna el proximo identificador valido para una pelicula"""
+        """Retorna el proximo identificador valido para una pelicula, o 1 si no hay peliculas"""
         if self.items:
             return self.items[-1].identifier + 1
         else:
