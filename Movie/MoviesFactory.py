b1d33d5f6d21320d2bfe14969ccfdac337ee7cdc
Roberto Alfonso <robertoa@prx.com.ar>
2019-07-16 14:07:02 -0300

    Removed unused library, comments, added docstring

--- Movie/MoviesFactory.py
+++ Movie/MoviesFactory.py
@@ -1,15 +1,12 @@
-import json
 from Movie.Movie import Movie
 
 class MoviesFactory:
-#    @staticmethod
-#    def create() -> Movie:
-#        return Movie()
-
     @staticmethod
     def create1(identifier: int, title: str, description: str, releasedate: str, director: str, category: str) -> Movie:
+        """Crea una pelicula con los atributos indicados"""
         return Movie(identifier, title, description, releasedate, director, category)
 
     @staticmethod
     def create(dictionary: dict) -> Movie:
+        """Crea una pelicula con los atributos indicados en el diccionario"""
         return Movie(dictionary=dictionary)
