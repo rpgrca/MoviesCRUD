f280b918a7b12931aad73b53e79b5321f3e78e9f
Roberto <reybrujo@gmail.com>
2019-07-25 01:14:22 -0300

    Cambios para agregar campo identifier

--- Manager/ContentProvider/SQLiteContentProvider.py
+++ Manager/ContentProvider/SQLiteContentProvider.py
@@ -32,20 +32,19 @@ class SQLiteContentProvider(ContentProvider):
             categories = {}
             self.__connection = sqlite3.connect(self.__filename)
             self.__connection.execute('CREATE TABLE IF NOT EXISTS Categories (id INTEGER PRIMARY KEY, name varchar(100) NOT NULL)')
-            self.__connection.execute('CREATE TABLE IF NOT EXISTS Movies (id INTEGER PRIMARY KEY, title varchar(100) NOT NULL, description varchar(1024) NOT NULL, releasedate TEXT NOT NULL, director varchar(100) NOT NULL, category INTEGER NOT NULL, FOREIGN KEY(category) REFERENCES categories(id))')
+            self.__connection.execute('CREATE TABLE IF NOT EXISTS Movies (id INTEGER PRIMARY KEY, identifier INTEGER, title varchar(100) NOT NULL, description varchar(1024) NOT NULL, releasedate TEXT NOT NULL, director varchar(100) NOT NULL, category INTEGER NOT NULL, FOREIGN KEY(category) REFERENCES categories(id))')
             self.__connection.commit()
 
             cursor = self.__connection.cursor()
-            for row in cursor.execute('SELECT * FROM Categories'):
-                categories[row[0]] = row[1]
+            for (id, name) in cursor.execute('SELECT id, name FROM Categories'):
+                self.categories.append(name)
 
             cursor.close()
 
             cursor = self.__connection.cursor()
-            for row in cursor.execute('SELECT * FROM Movies'):
-                self.movies.append(MoviesFactory.create1(int(row[0]), row[1], row[2], row[3], row[4], categories[row[5]]))
+            for row in cursor.execute('SELECT identifier, title, description, releasedate, director, Categories.name FROM Movies, Categories WHERE Movies.category = Categories.id'):
+                self.movies.append(MoviesFactory.create1(int(row[0]), row[1], row[2], row[3], row[4], row[5]))
 
-            self.categories = list(categories.values())
             cursor.close()
 
     def save(self):
@@ -53,22 +52,22 @@ class SQLiteContentProvider(ContentProvider):
         if self.__connection:
             cursor = self.__connection.cursor()
             try:
-                cursor.execute("DELETE FROM Movies")
+                cursor.execute("DELETE FROM Movies") # TODO: En un mundo ideal no se borra toda la lista
 
                 for movie in self.movies:
-                    cursor.execute("SELECT id FROM Categories WHERE name LIKE '{}'".format(movie.category))
+                    cursor.execute("SELECT id FROM Categories WHERE name LIKE ?", (movie.category,))
                     result = cursor.fetchone()
                     if not result:
-                        cursor.execute("INSERT INTO Categories (name) VALUES ('{}')".format(movie.category))
+                        cursor.execute("INSERT INTO Categories (name) VALUES (?)", (movie.category,))
                         category_id = cursor.lastrowid
                     else:
                         category_id = result[0]
 
-                    cursor.execute("INSERT INTO Movies (title, description, releasedate, director, category) VALUES ('{}', '{}', '{}', '{}', {})".format(movie.title, movie.description, movie.releasedate, movie.director, category_id))
+                    cursor.execute("INSERT INTO Movies (identifier, title, description, releasedate, director, category) VALUES (?, ?, ?, ?, ?, ?)", (movie.identifier, movie.title, movie.description, movie.releasedate, movie.director, category_id))
 
                 self.__connection.commit()
                 cursor.close()
             except sqlite3.IntegrityError:
-                print("El ID ya existe en la base de datos")
+                raise ValueError("El ID ya existe en la base de datos")
 
             self.__connection.close()
