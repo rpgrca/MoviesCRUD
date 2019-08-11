10a54a67dbc1bc60a230156b5e3fbae002a76d44
Roberto <reybrujo@gmail.com>
2019-08-11 19:06:03 -0300

    Cleaned indentation and spaces

--- Manager/ContentProvider/MemoryContentProvider.py
+++ Manager/ContentProvider/MemoryContentProvider.py
@@ -9,12 +9,12 @@ class MemoryContentProvider(ContentProvider):
     def __init__(self):
         """Constructor"""
         super(MemoryContentProvider, self).__init__()
-        self.__default_categories = [ "Accion", "Comedia", "Suspenso", "Terror" ]
-        self.__default_movies = [ Movie(1, "The Terminator", "American science fiction film", '1984-10-26', "James Cameron", "Accion"),
-                          Movie(2, "RoboCop", "Crime/Sci-fi", '1987-09-17', "Paul Verhoeven", "Accion"),
-                          Movie(3, "A Nightmare on Elm Street", "Mystery/Slasher", '1984-11-09', "Wes Craven", "Suspenso"),
-                          Movie(4, "Friday the 13th", "Slasher film", '1980-05-09', "Sean S. Cunningham", "Terror"),
-                          Movie(5, "Back to the Future", "Fantasy/Sci-fi", '1985-12-26', "Robert Zemeckis", "Comedia") ]
+        self.__default_categories = ["Accion", "Comedia", "Suspenso", "Terror"]
+        self.__default_movies = [Movie(1, "The Terminator", "American science fiction film", '1984-10-26', "James Cameron", "Accion"),
+                                 Movie(2, "RoboCop", "Crime/Sci-fi", '1987-09-17', "Paul Verhoeven", "Accion"),
+                                 Movie(3, "A Nightmare on Elm Street", "Mystery/Slasher", '1984-11-09', "Wes Craven", "Suspenso"),
+                                 Movie(4, "Friday the 13th", "Slasher film", '1980-05-09', "Sean S. Cunningham", "Terror"),
+                                 Movie(5, "Back to the Future", "Fantasy/Sci-fi", '1985-12-26', "Robert Zemeckis", "Comedia")]
         self.categories = self.__default_categories
         self.movies = self.__default_movies
         self.name = "Memoria Volatil"
